3c592682af72a23f5cb0c0cf160033d35ce73080
Antonio Locane <a.locane@eco-mind.eu>
2020-10-18 19:24:43 +0200

    added bootstrap parameters

--- dep-bootstrap.sh
+++ dep-bootstrap.sh
@@ -5,17 +5,18 @@ scriptVersion=0.3.0-SNAPSHOT
 
 version="$1"
 if [[ -z $version ]] ; then
-    >&2 echo "usage: $scriptName <version>"
+    >&2 echo "usage: $scriptName <version> [parameters]"
     exit 1
 fi
+shift
 
 targetDir="$HOME/.dep"
 if [[ ! -d "$targetDir/bootstrap/$version" ]] ; then
-    >&2 echo "dep-bootstrap version $version not found"
+    >&2 echo "bootstrap version $version not found"
     if [[ ! -d "$targetDir/git" ]] ; then
         >&2 echo "cloning dep-bootstrap repo tag $version in $targetDir/git"
         url=https://github.com/EcoMind/dep-bootstrap.git
-        git -c advice.detachedHead=false clone --depth 1 --branch "$1" "$url" "$targetDir/git" || exit 1
+        git -c advice.detachedHead=false clone --depth 1 --branch "$version" "$url" "$targetDir/git" || exit 1
     else
         >&2 echo "checking out dep-bootstrap repo tag $version in $targetDir/git"
         (cd "$targetDir/git" && git fetch --all --tags --prune && git reset --hard "tags/$version") || exit 1
@@ -27,4 +28,4 @@ fi
 
 >&2 echo "sourcing $targetDir/bootstrap/$version/bootstrap.sh"
 # shellcheck disable=SC1090
-. "$targetDir/bootstrap/$version/bootstrap.sh"
+. "$targetDir/bootstrap/$version/bootstrap.sh" "$@"
