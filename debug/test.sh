60866401627c03d8f8c037ecd2a021a051db594f
Antonio Locane <a.locane@eco-mind.eu>
2021-01-26 14:39:05 +0100

    removed unused files

--- debug/test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-PACKAGE_UNDER_TEST=/path/to/git/project
-PACKAGE_UNDER_TEST_DEPENDENCIES=log2/shell-common
-FILE_UNDER_TEST=versions
-
-. ./test-init.sh
-
-check_version /Users/alocane/_all/git/ecomind/eye4task-openvidu
